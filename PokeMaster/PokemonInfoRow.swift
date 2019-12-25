b6512c77f96d1aec61f523b0625b4757143e60e9
Willie <willie.wangwei@gmail.com>
2019-12-25 16:49:28 +0800

    cell增加展开功能并添加动画

--- PokeMaster/PokemonInfoRow.swift
+++ PokeMaster/PokemonInfoRow.swift
@@ -10,7 +10,8 @@ import SwiftUI
 
 struct PokemonInfoRow: View {
     
-    let model = PokemonViewModel.sample(id: 1)
+    let model: PokemonViewModel
+    @State var expanded: Bool
     
     var body: some View {
         
@@ -35,28 +36,75 @@ struct PokemonInfoRow: View {
             }
             .padding(.top, 12)
             
-            HStack(spacing: 20) {
+            Spacer()
+            
+            HStack(spacing: expanded ? 20 : -30) {
                 Spacer()
                 Button(action: {}) {
                     Image(systemName: "star")
-                        .font(.system(size: 25))
-                        .foregroundColor(.white)
-                        .frame(width: 30, height: 30)
+                        .modifier(ToolButtonModifier())
                 }
                 Button(action: {}) {
                     Image(systemName: "chart.bar")
-                        .font(.system(size: 25))
-                        .foregroundColor(.white)
-                        .frame(width: 30, height: 30)
+                        .modifier(ToolButtonModifier())
                     
                 }
                 Button(action: {}) {
-                    Image(systemName: "info.circle") .font(.system(size: 25)) .foregroundColor(.white)
-                        .frame(width: 30, height: 30)
+                    Image(systemName: "info.circle")
+                        .modifier(ToolButtonModifier())
                 }
             }
-            
+            .padding(.bottom, 12)
+            .opacity(expanded ? 1.0 : 0.0)
+            .frame(maxHeight: expanded ? .infinity : 0)
         }
-        .background(Color.green)
+        .frame(height: expanded ? 120 : 80)
+        .padding(.leading, 23)
+        .padding(.trailing, 15)
+        .background(
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(model.color, style: StrokeStyle(lineWidth: 4))
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [.white, model.color]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                )
+            }
+        )
+            .padding(.horizontal)
+            .animation(
+                .spring(
+                    response: 0.55,
+                    dampingFraction: 0.425,
+                    blendDuration: 0
+                )
+        )
+            .onTapGesture {
+                self.expanded.toggle()
+        }
+    }
+}
+
+struct ToolButtonModifier: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        return content
+            .font(.system(size: 25))
+            .foregroundColor(.white)
+            .frame(width: 30, height: 30)
+    }
+}
+
+struct PokemonInfoRow_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            PokemonInfoRow(model: .sample(id: 1), expanded: false)
+            PokemonInfoRow(model: .sample(id: 21), expanded: true)
+            PokemonInfoRow(model: .sample(id: 25), expanded: false)
+        }
     }
 }
